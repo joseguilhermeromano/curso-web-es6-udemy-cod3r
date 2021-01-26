8c8e1ca7ec6b3e6d64394dab73d69e292d174b85
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2021-01-26 02:40:27 -0300

    finalizacao do capitulo de HTML com assuntos de slider, imagens, videos e controladores, formulario e tabelas.

---
+++ html/exercicios/form/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const { response } = require('express')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/usuarios', (req, res) => {
+    console.log(req.body);
+    res.send('<h1>Parabéns. Usuário incluído!!</h1>');
+})
+
+app.post('/usuarios/:id', (req, res) => {
+    console.log(req.params.id)
+    console.log(req.body);
+    res.send('<h1>Parabéns. Usuário Alterado!!</h1>');
+})
+
+app.listen(3003);

0b0841969627c8424337342cafdf7f265866d7ae
José Guilherme Romano <jose_guilherme_romano@hotmail.com>
2022-02-22 21:41:04 -0300

    Conclusão do módulo de Projeto Calculadora em REACT.

---
+++ react/calculadora/src/components/Button.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Button.css'
+
+export default function Button(props) {
+    
+    let classes = 'button '
+    classes += props.operation ? 'operation' : ''
+    classes += props.double ? 'double' : ''
+    classes += props.triple ? 'triple' : ''
+
+    console.log(classes)
+
+    return (
+        <button 
+            onClick={e => props.click && props.click(props.label)}
+            className={classes}>
+            {props.label}
+        </button>
+    )
+}
